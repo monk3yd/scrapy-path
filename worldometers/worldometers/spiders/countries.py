acb6d0f1e4e1c00af1e3cb56d500d27ff8301bf0
monk3yd <jegajardog@gmail.com>
2022-04-16 04:05:08 -0400

    Scrape country name and link

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -7,9 +7,12 @@ class CountriesSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        countries = response.xpath("//td/a/text()").getall()
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
 
-        yield {
-            'title': title,
-            'countries': countries
-        }
+            yield {
+                'country_name': name,
+                'country_link': link
+            }
