79898203e0e88c92c59189a4284cf61394ede19b
monk3yd <jegajardog@gmail.com>
2022-04-16 04:45:21 -0400

    Scrape population per year data

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,4 +1,5 @@
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -15,4 +16,15 @@ class CountriesSpider(scrapy.Spider):
             # absolute_url = f"https://www.worldometers.info{link}"
             # absolute_url = response.urljoin(link)
 
-            yield response.follow(url=link)
+            yield response.follow(url=link, callback=self.parse_country)
+
+
+    def parse_country(self, response):
+        rows = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+        for row in rows:
+            year = row.xpath('.//td[1]/text()').get()
+            population = row.xpath('.//td[2]/strong/text()').get()
+            yield {
+                'year': year,
+                'population': population
+            }
