3153eb5ce19f9aa2ea885e5d9528950415d6fb9a
monk3yd <jegajardog@gmail.com>
2022-04-16 07:23:04 -0400

    Scrape debt to gdp ratio exercise, working

--- national_debt/national_debt/spiders/gdp_debt.py
+++ national_debt/national_debt/spiders/gdp_debt.py
@@ -4,8 +4,15 @@ import scrapy
 
 class GdpDebtSpider(scrapy.Spider):
     name = 'gdp_debt'
-    allowed_domains = ['www.worldpopulationreview.com']
-    start_urls = ['http://www.worldpopulationreview.com/countries/countries-by-national-debt/']
+    allowed_domains = ['www.worldpopulationreview.com', 'worldpopulationreview.com']
+    start_urls = ['https://worldpopulationreview.com/countries/countries-by-national-debt']
 
     def parse(self, response):
-        pass
+        countries = response.xpath("//table[@class='jsx-130793 table table-striped tp-table-body']/tbody/tr")
+        for country in countries:
+            name = country.xpath(".//td/a/text()").get()
+            debt_to_gdp_ratio = country.xpath(".//td[2]/text()").get()
+            yield {
+                'name': name,
+                'debt_to_gdp_ratio': debt_to_gdp_ratio
+            }            
