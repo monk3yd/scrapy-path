195565b6b0ba909e005a8d9dc5e04d27439523a1
monk3yd <jegajardog@gmail.com>
2022-04-18 06:53:40 -0400

    Parse item data

--- imdb/imdb/spiders/best_movies.py
+++ imdb/imdb/spiders/best_movies.py
@@ -16,4 +16,11 @@ class BestMoviesSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        print(response.url)
+        yield {
+            "title": response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            "year": response.xpath("//span[@id='titleYear']/a/text()").get(),
+            "duration": response.xpath("(//time)[1]/text()").get(),
+            "genre": response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            "rating": response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            "movie_url": response.url,
+        }
