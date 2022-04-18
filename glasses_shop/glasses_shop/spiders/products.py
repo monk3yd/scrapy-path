11c4b5608e2f10077fb027b77322a03794f108be
monk3yd <jegajardog@gmail.com>
2022-04-18 03:45:03 -0400

    Scrape product url and product image link, working

--- glasses_shop/glasses_shop/spiders/products.py
+++ glasses_shop/glasses_shop/spiders/products.py
@@ -4,8 +4,17 @@ import scrapy
 
 class ProductsSpider(scrapy.Spider):
     name = 'products'
-    allowed_domains = ['www.glassesshop.com/bestsellers']
+    allowed_domains = ['www.glassesshop.com', 'glassesshop.com']
     start_urls = ['https://www.glassesshop.com/bestsellers']
 
     def parse(self, response):
-        pass
+        products = response.xpath("//div[@id='product-lists']/div")
+        for product in products:
+            product_url = product.xpath(".//div[@class='product-img-outer']/a[1]/@href").get()
+            product_image_link = product.xpath(".//div[@class='product-img-outer']/a[1]/img/@data-src").get()
+            yield {
+                "url": product_url,
+                "image": product_image_link
+            }
+            # product_name =
+            # product_price =
