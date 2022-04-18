d4dfac104dec4f5003b3ddf5ad1576619d78a3cf
monk3yd <jegajardog@gmail.com>
2022-04-18 06:27:53 -0400

    Extract all items urls

--- imdb/imdb/spiders/best_movies.py
+++ imdb/imdb/spiders/best_movies.py
@@ -6,19 +6,14 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
-    allowed_domains = ['imdb.com']
-    start_urls = ['https://imdb.com']
+    allowed_domains = ['web.archive.org']
+    start_urls = ['http://web.archive.org/web/20200715000935/https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-        # Rule(LinkExtractor(deny=r'Items/'), callback='parse_item', follow=True),
-        Rule(LinkExtractor(restrict_xpaths=r'Items/'), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        # Allowed scrapable links
+        # XPath
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        print(response.url)
