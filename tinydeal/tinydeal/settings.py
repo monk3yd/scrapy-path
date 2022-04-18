6e3ea3a529890e5ed444463177167f2ff3c79690
monk3yd <jegajardog@gmail.com>
2022-04-18 02:21:51 -0400

    Overwrite user-agent request header value

--- tinydeal/tinydeal/settings.py
+++ tinydeal/tinydeal/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'tinydeal.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tinydeal (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
