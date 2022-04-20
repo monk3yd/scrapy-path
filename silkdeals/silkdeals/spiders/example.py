cf45f7ff189ce03615054a985c3146a1cbc7646d
monk3yd <jegajardog@gmail.com>
2022-04-19 21:04:58 -0400

    Exactly as scrapy-selenium docs with logger

--- silkdeals/silkdeals/spiders/example.py
+++ silkdeals/silkdeals/spiders/example.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
+import logging
 import scrapy
+from scrapy.utils.log import configure_logging
 # from scrapy.shell import inspect_response
 from scrapy_selenium import SeleniumRequest
 
 
 class ExampleSpider(scrapy.Spider):
     name = 'example'
-    
+
     # allowed_domains = ['duckduckgo.com', 'www.duckduckgo.com']
     # start_urls = ['https://duckduckgo.com']
 
-    url = 'https://duckduckgo.com'
+    # Output log to file
+    configure_logging(install_root_handler=False)
+    logging.basicConfig(
+        filename='log.txt',
+        format='%(levelname)s: %(message)s',
+        level=logging.INFO
+    )
 
     def start_request(self):
         yield SeleniumRequest(
-            url=url,
+            url='https://duckduckgo.com',
             callback=self.parse_result,
             wait_time=3,
             screenshot=True,
