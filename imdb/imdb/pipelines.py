57650e1716879b2799d4a107553e3dcd4d8053b2
monk3yd <jegajardog@gmail.com>
2022-04-22 04:27:05 -0400

    Cluster ready code

--- imdb/imdb/pipelines.py
+++ imdb/imdb/pipelines.py
@@ -5,19 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
+import pymongo
 
 
-class ImdbPipeline(object):
-
-    @classmethod  # Let you access values from settings.py
-    def from_crawler(cls, crawler):
-        logging.warning(crawler.settings.get("MONGO_URI"))
+class MongodbPipeline(object):
+    collection_name = "best_movies"
+    # @classmethod  # Let you access values from settings.py
+    # def from_crawler(cls, crawler):
+    #     logging.warning(crawler.settings.get("MONGO_URI"))
 
     def open_spider(self, spider):
-        logging.warning("SPIDER OPENED FROM PIPELINE")
+        # logging.warning("SPIDER OPENED FROM PIPELINE")
+        self.client = pymongo.MongoClient("")
+        self.db = self.client["IMDB"]
 
     def close_spider(self, spider):
-        logging.warning("SPIDER CLOSED FROM PIPELINE")
+        # logging.warning("SPIDER CLOSED FROM PIPELINE")
+        self.client.close()
 
     def process_item(self, item, spider):
+        # Insert into db
+        self.db[self.collection_name].insert(item)
         return item
