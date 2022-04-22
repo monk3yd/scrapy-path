4c6485c888f7783080f805c0c98b49ef3ce07902
monk3yd <jegajardog@gmail.com>
2022-04-21 20:21:10 -0400

    Pipeline, crawler get value from settings.py, create requirements.txt

--- imdb/imdb/pipelines.py
+++ imdb/imdb/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
 
 
 class ImdbPipeline(object):
+
+    @classmethod  # Let you access values from settings.py
+    def from_crawler(cls, crawler):
+        logging.warning(crawler.settings.get("MONGO_URI"))
+
+    def open_spider(self, spider):
+        logging.warning("SPIDER OPENED FROM PIPELINE")
+
+    def close_spider(self, spider):
+        logging.warning("SPIDER CLOSED FROM PIPELINE")
+
     def process_item(self, item, spider):
         return item
