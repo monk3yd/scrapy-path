a27d4bcd094912217e13f4a8403eee61bb29953a
monk3yd <jegajardog@gmail.com>
2022-04-22 20:51:55 -0400

    Scrape API with native scrapy

--- demo_api/demo_api/spiders/quotes.py
+++ demo_api/demo_api/spiders/quotes.py
@@ -16,3 +16,11 @@ class QuotesSpider(scrapy.Spider):
                 "quote_text": quote["text"],
                 "tags": quote["tags"],
             }
+
+        has_next = resp["has_next"]
+        if has_next:
+            next_page_number = resp["page"] + 1
+            yield scrapy.Request(
+                url=f"http://quotes.toscrape.com/api/quotes?page={next_page_number}",
+                callback=self.parse,
+            )
